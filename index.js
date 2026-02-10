cdddc0e255ba8e5f50711717de19da81757eee72
Samuel Brown <samuelwbrown4@gmail.com>
2026-02-10 12:45:58 -0500

    increase rate limit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 1000,
     message: {
         error: 'Too many requests from this IP, try again later.'
     },
